0704a832ae0ae4133083a06741b1df3eae86c260
Arnav Panicker <117341727+addu2612@users.noreply.github.com>
2023-12-12 23:13:32 +0530

    Update Experiment10.c

--- Experiment10.c
+++ Experiment10.c
@@ -1,42 +1,95 @@
-#include<stdio.h>
+//linear probing
+#include <stdio.h>
 #define SIZE 10
 
-int hash(int key){
-    return key%SIZE;
+int hash(int key) {
+    return key % SIZE;
 }
 
-int probe(int H[],int key){
-    int index=hash(key);
-    int i=0;
-    while(H[(index+i)%SIZE]!=0){
+int probe(int H[], int key) {
+    int index = hash(key);
+    int i = 0;
+    while (H[(index + i) % SIZE] != 0) {
         i++;
     }
-    return (index+i)%SIZE;
+    return (index + i) % SIZE;
 }
 
-void Insert(int H[], int key){
+void Insert(int H[], int key) {
     int index = hash(key);
-    if(H[index] != 0){
+    if (H[index] != 0) {
         index = probe(H, key);
     }
-    H[index] = key;  
+    H[index] = key;
 }
 
-int Search(int H[], int key){
-    int index=hash(key);
-    int i=0;
-    while(H[index+i]%SIZE!=key){
+int Search(int H[], int key) {
+    int index = hash(key);
+    int i = 0;
+    while (H[(index + i) % SIZE] != 0) {
+        if (H[(index + i) % SIZE] == key) {
+            return (index + i) % SIZE;
+        }
         i++;
     }
-    return (index+i)%SIZE;
+    return -1;
 }
 
-int main(){
-    int HT[10]={0};
-    Insert(HT,12);
-    Insert(HT,25);
-    Insert(HT,56);
-    Insert(HT,35);
+void display(int H[]) {
+    int i;
+    printf("\nIndex\tValue\n");
+    for (i = 0; i < SIZE; i++)
+        printf("%d\t%d\n", i, H[i]);
+}
+
+void displayMenu() {
+    printf("\nHash Table Operations:");
+    printf("\n1. Insert");
+    printf("\n2. Search");
+    printf("\n3. Display");
+    printf("\n4. Exit");
+    printf("\nEnter your choice: ");
+}
+
+int main() {
+    int HT[SIZE] = {0};
+    int choice, key, result;
+
+    do {
+        displayMenu();
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the key to insert: ");
+                scanf("%d", &key);
+                Insert(HT, key);
+                break;
+
+            case 2:
+                printf("Enter the key to search: ");
+                scanf("%d", &key);
+                result = Search(HT, key);
+                if (result != -1 && HT[result] == key) {
+                    printf("Element %d found at index %d\n", key, result);
+                } else {
+                    printf("Element not found\n");
+                }
+                break;
+
+            case 3:
+                display(HT);
+                break;
+
+            case 4:
+                printf("Exiting program.\n");
+                break;
+
+            default:
+                printf("Invalid choice! Please enter a valid option.\n");
+        }
+
+    } while (choice != 4);
 
     return 0;
 }
