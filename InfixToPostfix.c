5761639bd856ad1dd4be9ae6e1d9fad2aa429b07
Arnav Panicker <117341727+addu2612@users.noreply.github.com>
2024-01-07 20:52:13 +0530

    Update InfixToPostfix.c

--- InfixToPostfix.c
+++ InfixToPostfix.c
@@ -1,86 +1,114 @@
-// Implementation of Infix to Postfix Transformation
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
-char infix[100], postfix[100], st[100];
+
+char st[100];
+char infix[100], postfix[100];
 int top = -1;
+
 void push(char a)
 {
-    top++;
-    st[top] = a;
+    if (top == 99)
+    {
+        printf("Stack is full");
+    }
+    else
+    {
+        st[++top] = a;
+        printf("%c pushed into stack\n", a);
+    }
 }
+
 char pop()
 {
-    char a = st[top];
-    top--;
-    return a;
+    if (top == -1)
+    {
+        printf("Stack is empty\n");
+        return '\0';
+    }
+    else
+    {
+        char k = st[top];
+        top--;
+        return k;
+    }
 }
+
 int priority(char a)
 {
-    if (a == '%' || a == '^')
+    if (a == '^' || a == '%')
+    {
         return 3;
-    else if (a == '*' || a == '/')
+    }
+    else if (a == '/' || a == '*')
+    {
         return 2;
-else if(a=='+'|| a=='-')
-return 1;
-else{
-        return 0;
+    }
+    else if (a == '+' || a == '-')
+    {
+        return 1;
+    }
+    return 0;
 }
-}
+
 void infixtopost()
 {
-    int i = 0;
-    int j = 0;
+    int i = 0, j = 0;
     char x, y;
-    while (infix[i] != '\0')
+
+    x = infix[i];
+    while (x != '\0')
     {
-        x = infix[i];
         if (isalpha(x) || isdigit(x))
         {
             postfix[j] = x;
             j++;
         }
+
         else if (x == '(')
         {
             push(x);
         }
-else if( x=='+'|| x=='-'||x=='/'||x=='*'||x=='^'||x=='%'){
-while( top!=-1 && (priority(x)<=priority(st[top]))){
+        else if (x == '+' || x == '-' || x == '*' || x == '/' || x == '^' || x == '%')
+        {
+            while (top != -1 && priority(x) <= priority(st[top]))
+            {
+                y = pop();
+                postfix[j] = y;
+                j++;
+            }
+            push(x);
+        }
+        else if (x == ')')
+        {
             y = pop();
-            postfix[j] = y;
-            j++;
-}
-push(x);
+            while (y != '(')
+            {
+                postfix[j] = y;
+                j++;
+                y = pop();
+            }
+        }
+        else
+        {
+            printf("Expression is not correct");
+        }
+        i++;
+        x = infix[i];
     }
-    else if (x == ')')
+    while (top != -1)
     {
         y = pop();
-        while (y != '(')
-        {
-            postfix[j] = y;
-            j++;
-            y = pop();
-        }
+        postfix[j] = y;
+        j++;
     }
-    else
-    {
-        printf("Expression not correct!");
-    }
-    i++;
 }
-while (top != -1)
-{
-    y = pop();
-    postfix[j] = y;
-    j++;
-}
-}
-void main()
+
+int main()
 {
     printf("Enter the infix expression:\n");
-    scanf("%s", &infix);
+    scanf("%s", infix);
     printf("Infix Statement:\n%s\n", infix);
     infixtopost();
     printf("Postfix Statement:\n%s\n", postfix);
+    return 0;
 }
